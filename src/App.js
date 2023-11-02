f729ead61cb2db8160210e76b53a2bbaca3732a2
jella <dpdnjs7708@gmail.com>
2023-11-02 13:13:30 +0900

    회원가입 페이지 약관, 입력 분리 및 정규식 완료 (스프링부트 연결 전 단계)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import { Route, Routes } from 'react-router-dom';
 import Join from './JoinLogin/Join';
 import { useState } from 'react';
 import EmailJoin from './JoinLogin/EmailJoin';
+import JoinTemsPage from './JoinLogin/JoinTemsPage';
+
+
 
 function App() {
 
   return (
     <div className="App">
+    
+    {/* {false && <Header/>} */}
     <Header/>
     <Routes>
       {/* <Route path = '/login' element={<Login userInfo = {userInfo} 
@@ -17,6 +22,7 @@ function App() {
                                                setImsiName = {setImsiName}/>}/> */}
       <Route path='/join' element={<Join/>}/>
       <Route path='/emailJoin' element={<EmailJoin/>}/>
+      <Route path='/tems' element={<JoinTemsPage/>}/>
 
     </Routes>
     
