019a00bebd51e376676ceacfeb9ef9d6185d23d8
jella <dpdnjs7708.com>
2023-11-12 01:05:25 +0900

    funQ 오류 수정 및 DB저장 확인, footer추가

---
+++ src/FAQ/FAQ.js
@@ -0,0 +1,43 @@
+import { Accordion } from "react-bootstrap";
+import './FAQ.css';
+
+const FAQ = ()=>{
+  return(
+    <div className="FAQ-container">
+      <div className="faq-title">
+        <h3>자주 묻는 질문</h3>
+      </div>
+
+      <div>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+       </Accordion>
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
--- src/QComponent/Qtype.js
+++ src/QComponent/Qtype.js
@@ -6,7 +6,6 @@ import React, { useState } from 'react';
 
 function Qtype({ data , handleAddQ, surveyList, setSurveyList, type}) {
 
- console.log(type);
 
   return (
     <div className='DropdownContainer'>
